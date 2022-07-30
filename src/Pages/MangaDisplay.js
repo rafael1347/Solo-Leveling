e9c631e8e947eff210ecf95a26c3f853742da115
rafael1347 <rafaelm_0495@hotmail.com>
2022-07-30 12:31:12 -0400

    function to make pages array

--- src/Pages/MangaDisplay.js
+++ src/Pages/MangaDisplay.js
@@ -1,10 +1,35 @@
+import { useEffect, useState } from "react";
 import MeetupList from "../Components/meetups/MeetupList";
 import { Dummy_Data } from "../Data/DummyData";
+
 const MangaDisplay = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadedPages, setLoadedPages] = useState([]);
+
+  useEffect(() => {
+    setIsLoading(true);
+    let i = 1;
+    const pages = [];
+    while (i <= 27) {
+      const key = i;
+      const entry = `https://cdn.black-clover.org/file/sugois/solo-leveling/Chapter-1/${i}.jpg`;
+      ++i;
+      console.log(entry);
+      const page = {
+        id: key,
+        image: entry,
+      };
+      pages.push(page);
+    }
+
+    setIsLoading(false);
+    setLoadedPages(pages);
+  }, []);
+
   return (
     <section>
       <h1>You are now reading Solo Leveling</h1>
-      <MeetupList meetups={Dummy_Data} />
+      <MeetupList meetups={loadedPages} />
     </section>
   );
 };
